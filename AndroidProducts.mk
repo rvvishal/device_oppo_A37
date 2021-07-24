35223a83445762d8e3172790eb9599c53eaed3b9
Vishal Rajbhar <71400384+rvvishal@users.noreply.github.com>
2021-07-24 16:30:20 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_A37.mk
+    $(LOCAL_DIR)/lineage_A33.mk
 
 # Lunch choices
 COMMON_LUNCH_CHOICES := \
-    lineage_A37-user \
-    lineage_A37-userdebug \
-    lineage_A37-eng
+    lineage_A33-user \
+    lineage_A33-userdebug \
+    lineage_A33-eng
